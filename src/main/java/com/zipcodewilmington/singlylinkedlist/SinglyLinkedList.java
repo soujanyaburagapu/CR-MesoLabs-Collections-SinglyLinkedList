0a66a54ef0cf6e2f7ac15869fa8638aa3acd68f6
Soujanya Buragapu <soujanyaburagapu@zipcoders-MacBook-Pro-35.local>
2018-11-08 17:54:24 -0500

    Single Linked List

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -3,5 +3,205 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+
+public class SinglyLinkedList<T extends Comparable<T>> {
+
+    private Node head;
+    private Node tail;
+    private int nodeCount;
+
+    public SinglyLinkedList() {
+        head = null;
+        tail = null;
+        nodeCount = 0;
+    }
+
+    public void add(T data) {
+        if (head == null) {
+            head = new Node(data);
+            tail = head;
+        } else {
+            Node newTailNode = new Node(data);
+            tail.setNext(newTailNode);
+            tail = newTailNode;
+        }
+        nodeCount++;
+    }
+
+    public void insert(T data, int position) {
+        if (position > size()) {
+            throw new IllegalStateException("");
+        }
+
+        Node node = head;
+        for (int i = 1; i < position && node != null; i++) {
+            node = node.getNext();
+        }
+
+        Node newNode = new Node(data);
+        Node nextNode = node.getNext();
+        node.setNext(newNode);
+        newNode.setNext(nextNode);
+
+        nodeCount++;
+    }
+
+    public T removeAt(int position) {
+        if (position > size()) {
+            throw new IllegalStateException();
+        }
+
+        Node node = head;
+        Node prevNode = head;
+
+        for (int i = 1; i < position && node != null; i++) {
+            prevNode = node;
+            node = node.getNext();
+        }
+
+        T nodeData = node.getData();
+        prevNode.setNext(node.getNext());
+
+        nodeCount--;
+
+        return nodeData;
+    }
+
+    public T remove() {
+        if (head == null) {
+            throw new IllegalStateException();
+        }
+        T data = head.getData();
+        head = head.getNext();
+        nodeCount--;
+        return data;
+    }
+
+    public T get(int position) {
+        if (position > size()) {
+            throw new IllegalStateException();
+        }
+        Node node = head;
+        for (int i = 1; i < position && node != null; i++) {
+            node = node.getNext();
+        }
+        return node.getData();
+    }
+
+    public int find(T data) {
+        if (head == null) {
+            throw new IllegalStateException();
+        }
+        Node node = head;
+        for (int i = 1; i < size() && node != null; i++) {
+            T checkData = node.getData();
+            if (checkData.equals(data)) {
+                return i;
+            }
+            node = node.getNext();
+        }
+        return -1;
+    }
+
+    public boolean contains(T data) {
+        if (head == null) {
+            throw new IllegalStateException("");
+        }
+        Node node = head;
+        for (int i = 1; i < size() && node != null; i++) {
+            T checkData = node.getData();
+            if (checkData.equals(data)) {
+                return true;
+            }
+            node = node.getNext();
+        }
+        return false;
+    }
+
+    public SinglyLinkedList<T> copy() {
+        SinglyLinkedList<T> copy = new SinglyLinkedList<T>();
+
+        Node node = head;
+
+        for (int i = 1; i <= size() && node.getNext() != null; i++) {
+            copy.add(node.getData());
+            node = node.getNext();
+        }
+
+        copy.add(tail.getData());
+
+        return copy;
+    }
+
+    public void sort() {
+        if (head == null || head.getNext() == null) {
+            throw new IllegalStateException();
+        }
+
+        boolean wasChanged;
+
+        do {
+            Node node = head;
+            Node prevNode = null;
+            Node nextNode = head.next;
+            wasChanged = false;
+
+            while (nextNode != null) {
+                if (node.getData().compareTo(nextNode.getData()) > 0) {
+                    wasChanged = true;
+                    if (prevNode != null) {
+                        Node temp = nextNode.next;
+
+                        prevNode.next = nextNode;
+                        nextNode.next = node;
+                        node.next = temp;
+                    } else {
+                        Node temp = nextNode.next;
+
+                        head = nextNode;
+                        nextNode.next = node;
+                        node.next = temp;
+                    }
+                    prevNode = nextNode;
+                    nextNode = node.next;
+                } else {
+                    prevNode = node;
+                    node = nextNode;
+                    nextNode = nextNode.next;
+                }
+            }
+        } while (wasChanged);
+
+    }
+
+    public int size() {
+        return nodeCount;
+    }
+
+    public class Node {
+        private Node next;
+        private T data;
+
+        public Node(T data) {
+            this.next = null;
+            this.data = data;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+    }
+
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,154 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedListTest {
+public class SinglyLinkedListTest
+{
+    @Test
+    public void SinglyLinkedListTest() {
+        //Given
+        SinglyLinkedList linkedList = new SinglyLinkedList();
+        linkedList.add("Zip ");
+        linkedList.add("Code");
+        String test = "";
+        //When
+        for (int i = 0; i <= linkedList.size(); i++) {
+            String string = (String) linkedList.remove();
+            test = test.concat(string);
+        }
+        System.out.println(test);
+    }
+
+    @Test
+    public void SinglyLinkedListTest1() {
+        SinglyLinkedList linkedList = new SinglyLinkedList();
+        linkedList.add("Zip ");
+        linkedList.add(" Code ");
+        int expected = 2;
+        int actual = linkedList.size();
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void SinglyLinkedListTest2() {
+        SinglyLinkedList linkedList = new SinglyLinkedList();
+
+        linkedList.add("Z");
+        linkedList.add("i");
+        linkedList.add("p");
+        linkedList.add("");
+        linkedList.add("C");
+        linkedList.add("o");
+        linkedList.add("d");
+        linkedList.add("e");
+
+        int expected = 8;
+        int actual = linkedList.size();
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void SinglyLinkedListTest3() {
+        SinglyLinkedList linkedList = new SinglyLinkedList();
+
+        linkedList.add(" C ");
+        linkedList.add(" O ");
+        linkedList.add(" D ");
+        linkedList.add(" E ");
+
+        linkedList.insert("Zip", 1);
+
+        int expected = 5;
+        int actual = linkedList.size();
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void SinglyLinkedListTest4() {
+        SinglyLinkedList linkedList = new SinglyLinkedList();
+
+        linkedList.add("C");
+        linkedList.add("O");
+        linkedList.add("D");
+        linkedList.add("E");
+
+        String actual = (String) linkedList.removeAt(2);
+        String expected = "O";
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void SinglyLinkedListTest5() {
+        SinglyLinkedList linkedList = new SinglyLinkedList();
+
+        linkedList.add("C");
+        linkedList.add("O");
+        linkedList.add("D");
+        linkedList.add("E");
+
+        int actual = linkedList.find("D");
+        int expected = 3;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void SinglyLinkedListTest6() {
+        SinglyLinkedList linkedList = new SinglyLinkedList();
+
+        linkedList.add("list");
+        linkedList.add("linked");
+        linkedList.add("array");
+        int actual = linkedList.find("string");
+        int expected = -1;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void SinglyLinkedListTest7() {
+        SinglyLinkedList linkedList = new SinglyLinkedList();
+
+        linkedList.add("Zip ");
+        linkedList.add("Code");
+
+
+        boolean actual = linkedList.contains("Wilmington");
+        boolean expected = false;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void SinglyLinkedListTest8() {
+        SinglyLinkedList linkedList = new SinglyLinkedList();
+
+        linkedList.add("Zip ");
+        linkedList.add("Code ");
+        linkedList.add("Wilmington");
+
+        SinglyLinkedList actual = linkedList;
+        SinglyLinkedList expected = linkedList.copy();
+
+        for (int i = 1; i <= expected.size(); i++) {
+            System.out.println(expected.get(i));
+        }
+        expected.remove();
+        for (int i = 1; i <= expected.size(); i++) {
+            System.out.println(expected.get(i));
+        }
+        for (int i = 1; i < linkedList.size()+1; i++) {
+            System.out.println(linkedList.get(i));
+        }
+    }
 }
+
+
+
